0f53624be95357a91b3b0ef7dea62336b9ab67a1
TryitNow1 <108473546+TryitNow1@users.noreply.github.com>
2022-06-30 19:43:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+hamburgur=document.querySelector('.hamburger');
+navlist=document.querySelector('.list');
+navbar=document.querySelector('.navbar');
+
+hamburgur.addEventListener('click',()=>{
+    // console.log("click");
+    navlist.classList.toggle('navlistdis');
+    navbar.classList.toggle('navbarht');
+    hamburgur.classList.toggle('hamdis')
+})
+About=document.querySelector('#About');
+About.addEventListener('click',(About)=>{
+    // console.log("click");
+    navlist.classList.toggle('navlistdis');
+    navbar.classList.toggle('navbarht');
+    hamburgur.classList.toggle('hamdis')
+})
+Education=document.querySelector('#Education');
+Education.addEventListener('click',(Education)=>{
+    // console.log("click");
+    navlist.classList.toggle('navlistdis');
+    navbar.classList.toggle('navbarht');
+    hamburgur.classList.toggle('hamdis')
+})
+Skill=document.querySelector('#Skill');
+Skill.addEventListener('click',(Skill)=>{
+    // console.log("click");
+    navlist.classList.toggle('navlistdis');
+    navbar.classList.toggle('navbarht');
+    hamburgur.classList.toggle('hamdis')
+})
+Contact=document.querySelector('#Contact');
+Contact.addEventListener('click',(Contact)=>{
+    // console.log("click");
+    navlist.classList.toggle('navlistdis');
+    navbar.classList.toggle('navbarht');
+    hamburgur.classList.toggle('hamdis')
+})
